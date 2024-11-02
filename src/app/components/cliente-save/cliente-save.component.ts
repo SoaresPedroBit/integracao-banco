9ad848c679de063154050c56aa00f227a531ad37
Pedro Soares <soarespedroo357@gmail.com>
2024-11-01 21:57:36 -0300

    clienteSave

--- src/app/components/cliente-save/cliente-save.component.ts
+++ src/app/components/cliente-save/cliente-save.component.ts
@@ -1,26 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClienteService } from '../../services/cliente.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-cliente-save',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cliente-save.component.html',
   styleUrl: './cliente-save.component.scss'
 })
-export class ClienteSaveComponent {
-  clienteForm: FormGroup;
+export class ClienteSaveComponent implements OnInit{
+  clienteForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private clienteService: ClienteService) {
+  constructor(private fb: FormBuilder, private clienteService: ClienteService,private route:Router) {}
+
+  ngOnInit(): void {
     this.clienteForm = this.fb.group({
-      nome: ['', Validators.required],
-      cpf: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
-      email: ['', [Validators.required, Validators.email]],
+      nome: [''],
+      email: [''],
+      cpf: [''],
+      rg: [''],
+      profissao: [''],
       telefone: [''],
-      status: ['Ativo', Validators.required]
+      dataNascimento: [''],
+      estadoCivil: [''],
+      endereco: this.fb.group({
+        logradouro: [''],
+        numero: [''],
+        complemento: [''],
+        bairro: [''],
+        cidade: [''],
+        uf: [''],
+        cep: ['']
+      })
     });
   }
 
@@ -28,10 +45,19 @@ export class ClienteSaveComponent {
     if (this.clienteForm.valid) {
       this.clienteService.save(this.clienteForm.value).subscribe({
         next: (response) => {
-          console.log('Cliente salvo com sucesso:', response);
+          Swal.fire({
+            title:'Cadastrado com sucesso',
+            icon:'success',
+            confirmButtonText:'OK'
+          })
+          this.route.navigate(['/clientes']);
         },
-        error: (error) => {
-          console.error('Erro ao salvar cliente:', error);
+        error: erro =>{
+          Swal.fire({
+            title:'Erro ao salvar',
+            icon: 'error',
+            confirmButtonText:'OK'
+          })
         }
       });
     }
